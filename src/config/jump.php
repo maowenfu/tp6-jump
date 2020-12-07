23b57ba6f1d29fcc56b83a95a6f50d2960e028e9
maowenfu <357554469@qq.com>
2020-12-07 11:31:09 +0800

    修改配置文件地址

--- src/config/jump.php
+++ src/config/jump.php
@@ -2,11 +2,11 @@
 /**
  * @Author: Maowenfu
  * @Date:   2020-12-07 09:36:29
- * @Last Modified by:   admin
- * @Last Modified time: 2020-12-07 09:36:44
+ * @Last Modified by:   maowenfu
+ * @Last Modified time: 2020-12-07 11:11:11
  */
 return[
     // 默认跳转页面对应的模板文件
-    'dispatch_success_tmpl' => app()->getRootPath().'/vendor/maowenfu/thinkphp-jump/src/tpl/dispatch_jump.tpl',
-    'dispatch_error_tmpl'   => app()->getRootPath().'/vendor/maowenfu/thinkphp-jump/src/tpl/dispatch_jump.tpl',
+    'dispatch_success_tmpl' => app()->getRootPath().'/vendor/maowenfu/think-jump/src/tpl/dispatch_jump.tpl',
+    'dispatch_error_tmpl'   => app()->getRootPath().'/vendor/maowenfu/think-jump/src/tpl/dispatch_jump.tpl',
 ];

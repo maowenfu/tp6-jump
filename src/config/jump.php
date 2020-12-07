144014bc445d95759d075b1c06340be959d03854
maowenfu <357554469@qq.com>
2020-12-07 09:46:10 +0800

    代码提交

---
+++ src/config/jump.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @Author: Maowenfu
+ * @Date:   2020-12-07 09:36:29
+ * @Last Modified by:   admin
+ * @Last Modified time: 2020-12-07 09:36:44
+ */
+return[
+    // 默认跳转页面对应的模板文件
+    'dispatch_success_tmpl' => app()->getRootPath().'/vendor/maowenfu/thinkphp-jump/src/tpl/dispatch_jump.tpl',
+    'dispatch_error_tmpl'   => app()->getRootPath().'/vendor/maowenfu/thinkphp-jump/src/tpl/dispatch_jump.tpl',
+];
